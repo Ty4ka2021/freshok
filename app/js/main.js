a3bb8439f99335d53baf9f9e5db81017f2a5c825
Ty4ka2021 <dm.hmara@gmail.com>
2021-07-18 18:22:03 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $(function () {
-  
+  $('.slider__carousel').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>'
+  });
 })
--- gulpfile.js
+++ gulpfile.js
@@ -37,10 +37,12 @@ function images() {
 }
 
 function scripts() {
-    return src([
-        'node_modules/jquery/dist/jquery.js',
-        'app/js/main.js'
-    ])
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'app/js/main.js',
+    'node_modules/slick-carousel/slick/slick.js',
+
+  ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js'))
@@ -48,7 +50,11 @@ function scripts() {
 }
 
 function styles() {
-    return src('app/scss/style.scss')
+  return src([
+    'app/scss/style.scss',
+    'node_modules/slick-carousel/slick/slick.css',
+
+  ])
         .pipe(scss({outputStyle: 'compressed'}))
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
