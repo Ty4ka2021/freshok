b36ce677da191b9346661cf0e975e403fe30a7fd
Ty4ka2021 <dm.hmara@gmail.com>
2021-07-20 23:17:49 +0300

    top section

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,10 @@ $(function () {
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>'
   });
+
+
+
+
+
+  var mixer = mixitup('.top');
 })
--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'app/js/main.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/mixitup/dist/mixitup.js',
 
   ])
     .pipe(concat('main.min.js'))
