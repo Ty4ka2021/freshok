3f50db18c10f8ed9a283913fe4d92615bb04766c
Ty4ka2021 <dm.hmara@gmail.com>
2021-08-04 22:09:57 +0300

    dz 14

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,78 @@
 $(function () {
   $('.main-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><img src="images/sprite/arrow-left.svg" alt="стрелка влево"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/sprite/arrow-right.svg" alt="стрелка вправо"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="images/sprite/arrow-right.svg" alt="стрелка вправо"></button>',
+    // responsive: [
+    //   {
+    //     breakpoint: 1400,
+    //     settings: {
+    //       arrows: false,
+    //       dots: true,
+    //     }
+    //   }
+    // ]
+    // ломает изображения в слайдере при размере указаной ширине
   });
 
   $('.brands__box').slick({
     slidesToShow: 6,
     arrows: false,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 430,
+        settings: {
+          slidesToShow: 2,
+        }
+      }
+    ]
   });
 
   $('.user__link--cart').on('click', function () {
-    $('.cart').toggleClass('cart--active');
+    $('.cart').toggleClass('cart--active'),
+    $('body').addClass('body--lock');
   });
 
-  $('.cart__close').on('click', function () {
-    $('.cart').toggleClass('cart--active');
+  $('.close--cart').on('click', function () {
+    $('.cart').toggleClass('cart--active'),
+    $('body').removeClass('body--lock');
+  });
+
+  $('.header__burger').on('click', function () {
+    $('.menu').addClass('menu--active'),
+    $('body').addClass('body--lock');
+  });
+
+  $('.close--menu').on('click', function () {
+    $('.menu').removeClass('menu--active'),
+    $('body').removeClass('body--lock');
+  });
+
+  $('.user__item--search').on('click', function () {
+    $('.search').toggleClass('search--active');
+  });
+
+  $('.header__catalog').hover(function () {
+    $('.catalog').toggleClass('catalog--active');
   });
 
 
-  
 
+  var containerEl1 = document.querySelector('[data-ref="top-products"]');
+  var containerEl2 = document.querySelector('[data-ref="promo"]');
 
-  var mixer = mixitup('.top-products');
-  var mixer = mixitup('.promo');
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 })
